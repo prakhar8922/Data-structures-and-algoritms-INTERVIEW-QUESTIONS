693cd14b6ba9fe75c815d1575698ea416ba785f4
prakhar8922 <92254361+prakhar8922@users.noreply.github.com>
2022-11-12 19:27:27 +0530

    disjoint set again

--- graphs/undirected/Minimum Spanning Tree.cpp
+++ graphs/undirected/Minimum Spanning Tree.cpp
@@ -40,6 +40,67 @@
 // 1 ≤ w ≤ 1000
 // Graph is connected and doesn't contain self loops & multiple edges.
 
+// kruskal algo
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUPar(int node)
+    { // findUpar=find ultimate parent of node
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u); // ulp_u=ultimate parent of u
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
 class Solution
 {
 public:
@@ -47,29 +108,66 @@ public:
     int spanningTree(int v, vector<vector<int>> adj[])
     {
         // code here
-        int sum = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> vis(v, 0);
-        pq.push({0, 0}); // weight and node
-        while (!pq.empty())
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < v; i++)
         {
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            if (vis[node] == 1)
-                continue;  // if the node has already been visited
-            vis[node] = 1; // if the node has not been visited
-            sum += wt;
-            for (auto it : adj[node])
+            for (auto it : adj[i])
             {
+                int node = i;
                 int adjnode = it[0];
-                int edW = it[1];
-                if (!vis[adjnode])
-                {
-                    pq.push({edW, adjnode});
-                }
+                int wt = it[1];
+                edges.push_back({wt, {node, adjnode}});
             }
         }
-        return sum;
+        DisjointSet ds(v);
+        sort(edges.begin(), edges.end());
+        int mwt = 0;
+        for (auto it : edges)
+        {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            if (ds.findUPar(u) != ds.findUPar(v))
+            {
+                mwt += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+        return mwt;
     }
 };
+
+// PRIMS ALGO
+// class Solution
+// {
+// public:
+//     // Function to find sum of weights of edges of the Minimum Spanning Tree.
+//     int spanningTree(int v, vector<vector<int>> adj[])
+//     {
+//         // code here
+//         int sum = 0;
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//         vector<int> vis(v, 0);
+//         pq.push({0, 0}); // weight and node
+//         while (!pq.empty())
+//         {
+//             int wt = pq.top().first;
+//             int node = pq.top().second;
+//             pq.pop();
+//             if (vis[node] == 1)
+//                 continue;  // if the node has already been visited
+//             vis[node] = 1; // if the node has not been visited
+//             sum += wt;
+//             for (auto it : adj[node])
+//             {
+//                 int adjnode = it[0];
+//                 int edW = it[1];
+//                 if (!vis[adjnode])
+//                 {
+//                     pq.push({edW, adjnode});
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+// };
