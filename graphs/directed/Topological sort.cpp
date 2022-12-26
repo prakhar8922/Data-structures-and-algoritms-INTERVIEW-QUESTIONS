129a8b2a302833ecfd71ea55be8fdf329d1332cf
prakhar8922 <92254361+prakhar8922@users.noreply.github.com>
2022-12-26 22:13:22 +0530

    modified ans

--- graphs/directed/Topological sort.cpp
+++ graphs/directed/Topological sort.cpp
@@ -76,42 +76,42 @@ public:
     }
 };
 
-// class Solution
-// {
-// private:
-//     void dfs(int node, int vis[], stack<int> &st, vector<int> adj[])
-//     {
-//         vis[node] = 1;
-//         for (auto it : adj[node])
-//         {
-//             if (!vis[it])
-//             {
-//                 dfs(it, vis, st, adj);
-//             }
-//         }
-//         st.push(node);
-//     }
+class Solution
+{
+private:
+    void dfs(int node, int vis[], stack<int> &st, vector<int> adj[])
+    {
+        vis[node] = 1;
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                dfs(it, vis, st, adj);
+            }
+        }
+        st.push(node);
+    }
 
-// public:
-//     // Function to return list containing vertices in Topological order.
-//     vector<int> topoSort(int v, vector<int> adj[])
-//     {
-//         // code here
-//         vector<int> ans;
-//         int vis[v] = {0};
-//         stack<int> st;
-//         for (int i = 0; i < v; i++)
-//         {
-//             if (!vis[i])
-//             {
-//                 dfs(i, vis, st, adj);
-//             }
-//         }
-//         while (!st.empty())
-//         {
-//             ans.push_back(st.top());
-//             st.pop();
-//         }
-//         return ans;
-//     }
-// };
+public:
+    // Function to return list containing vertices in Topological order.
+    vector<int> topoSort(int v, vector<int> adj[])
+    {
+        // code here
+        vector<int> ans;
+        int vis[v] = {0};
+        stack<int> st;
+        for (int i = 0; i < v; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, vis, st, adj);
+            }
+        }
+        while (!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
--- graphs/undirected/547.number-of-provinces.cpp
+++ graphs/undirected/547.number-of-provinces.cpp
@@ -52,58 +52,74 @@ public:
         return count;
     }
 };
-// @lc code=end
-// class DisjointSet{
 
-//   public:
-//   vector<int>parent,size;
-//   DisjointSet(int n){
-//       parent.resize(n+1);
-//       size.resize(n+1);
-//       for(int i=0;i<=n;i++){
-//           parent[i]=i;
-//           size[i]=1;
-//       }
-//   }
-//   int findUpar(int node){
-//       if(node==parent[node]){
-//           return node;
-//       }
-//       return parent[node]=findUpar(parent[node]);
-//   }
-//   void unionBySize(int u,int v){
-//       int ulp_u=findUpar(u);
-//       int ulp_v=findUpar(v);
-//       if(ulp_u==ulp_v)return;
-//       if(size[ulp_u]<size[ulp_v]){
-//           parent[ulp_u]=ulp_v;
-//           size[ulp_v]+=size[ulp_u];
-//       }
-//       else{
-//           parent[ulp_v]=ulp_u;
-//           size[ulp_u]+=size[ulp_v];
-//       }
-//   }
-// };
+@lc code = end
 
-// class Solution {
-//   public:
-//     int numProvinces(vector<vector<int>> adj, int v) {
-//         // code here
-//         DisjointSet ds(v);
-//         for(int i=0;i<v;i++){
-//             for(int j=0;j<v;j++){
-//                 if(adj[i][j]==1){
-//                     ds.unionBySize(i,j);
-//                 }
-//             }
-//         }
-//         int cnt=0;
-//         for(int i=0;i<v;i++){
-//             if(ds.parent[i]==i)
-//                 cnt++;
+    class DisjointSet
+{
 
-//         }
-//         return cnt;
-//     }
-// };
+public:
+    vector<int> parent, size;
+    DisjointSet(int n)
+    {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUpar(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findUpar(parent[node]);
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution
+{
+public:
+    int numProvinces(vector<vector<int>> adj, int v)
+    {
+        // code here
+        DisjointSet ds(v);
+        for (int i = 0; i < v; i++)
+        {
+            for (int j = 0; j < v; j++)
+            {
+                if (adj[i][j] == 1)
+                {
+                    ds.unionBySize(i, j);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < v; i++)
+        {
+            if (ds.parent[i] == i)
+                cnt++;
+        }
+        return cnt;
+    }
+};
